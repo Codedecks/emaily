8de92a42efec40717b069181e7e86961f8d5a1ee
codedecks <codedecks@gmail.com>
2017-12-14 16:25:26 -0800

    update test

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// https://whispering-bastion-13555.herokuapp.com/
+// https://git.heroku.com/whispering-bastion-13555.git
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({hi: 'there'});
+  res.send({hi: 'there!'});
 });
 
 const PORT = process.env.PORT || 5000;
